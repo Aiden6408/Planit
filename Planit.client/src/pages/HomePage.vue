a27980d3d17feaddf468af266e84acfefcf6362e
Aiden Eskatakii <aidenjameseskatakii@gmail.com>
2022-03-15 13:29:13 -0600

    pus client push

--- Planit.client/src/pages/HomePage.vue
+++ Planit.client/src/pages/HomePage.vue
@@ -1,30 +1,47 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div
+    class="
+      home
+      flex-grow-1
+      d-flex
+      flex-column
+      align-items-center
+      justify-content-center
+    "
+  >
+    <div
+      v-if="!user.isAuthenticated"
+      class="home-card p-5 bg-primary text-light rounded elevation-3"
+    >
+      <h1>Please Login to Continue</h1>
     </div>
   </div>
 </template>
 
 <script>
+import { computed } from '@vue/reactivity'
+import { AppState } from '../AppState'
 export default {
-  name: 'Home'
+  setup() {
+
+    return {
+      user: computed(() => AppState.user)
+
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.home{
+.home {
   display: grid;
   height: 80vh;
   place-content: center;
   text-align: center;
   user-select: none;
-  .home-card{
+  .home-card {
     width: 50vw;
-    > img{
+    > img {
       height: 200px;
       max-width: 200px;
       width: 100%;
