52338a47e91c20821ed8ff63d3e5d27b545af849
Brian Crossley <bcrossley712@gmail.com>
2022-03-14 22:19:16 -0600

    update tasksService

--- Planit/server/services/TasksService.js
+++ Planit/server/services/TasksService.js
@@ -1,14 +1,18 @@
 import { dbContext } from "../db/DbContext"
 
 class TasksService {
-  async getAll(query) {
-    const tasks = await dbContext.  }
+  async getAll(query = {}) {
+    const tasks = await dbContext.Tasks
+  }
   async create(body) {
-    const tasks = await dbContext.  }
+    const tasks = await dbContext.Tasks
+  }
   async edit(body, id) {
-    const tasks = await dbContext.  }
+    const tasks = await dbContext.Tasks
+  }
   async remove(userId, id) {
-    const tasks = await dbContext.  }
+    const tasks = await dbContext.Tasks
+  }
 }
 
 export const tasksService = new TasksService()
